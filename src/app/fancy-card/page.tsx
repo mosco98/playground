02e204ac03f53864c6ee8b1ebd85b310f415a7be
Moses Enyinnaya <moscolyno98@gmail.com>
2024-08-27 08:30:26 +0200

    minor fix

--- src/app/fancy-card/page.tsx
+++ src/app/fancy-card/page.tsx
@@ -1,12 +1,71 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
+import { motion } from "framer-motion";
+import { useState } from "react";
+
 const FancyCard = () => {
   return (
-    <div className="min-h-screen grid place-content-center">
-      <div className="w-[280px] h-[380px] rounded-2xl overflow-hidden relative group cursor-pointer">
+    <div className="min-h-screen grid place-content-center bg-white">
+      <div className="flex items-center gap-10">
+        <Card
+          title="6 bedroom duplex"
+          instagram="@investorsabinus"
+          location="Port harcourt, Nigeria"
+          image="/house2.jpg"
+        />
+
+        <Card
+          status="unavailable"
+          title="4 bedroom bungalow"
+          instagram="@carlosinvestors"
+          location="Kigali, Rwanda"
+          image="/house.jpg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FancyCard;
+
+const Card = ({
+  status = "available",
+  title,
+  instagram,
+  location,
+  image
+}: {
+  status?: "available" | "unavailable";
+  title: string;
+  instagram: string;
+  location: string;
+  image: string;
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      className="w-[280px] h-[380px] relative"
+      onMouseOver={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <motion.p
+        initial={{ y: !isHovered ? 40 : 0, opacity: !isHovered ? 0 : 1 }}
+        animate={{
+          y: !isHovered ? 40 : 0,
+          opacity: !isHovered ? 0 : 1,
+          transition: { delay: isHovered ? 0.5 : 0 }
+        }}
+        transition={{ duration: 0.3 }}
+        className="text-4xl text-center mb-2.5"
+      >
+        {status === "unavailable" ? "😔" : "😄"}{" "}
+      </motion.p>
+
+      <div className="w-full h-full relative group cursor-pointer rounded-2xl overflow-hidden">
         <img
-          src="/house.jpg"
+          src={image}
           alt="house"
           className="absolute h-full w-full object-cover group-hover:scale-y-110 transition-all duration-500 ease-in-out origin-bottom will-change-transform"
         />
@@ -16,13 +75,12 @@ const FancyCard = () => {
             <div className="absolute inset-0 bg-orange-950/80"></div>
             <div className="relative px-4 pt-4 pb-7 bg-black/85 overflow-hidden">
               <div>
-                <h1 className="text-white tracking-tight font-medium text-lg">
-                  6 bedroom duplex
+                <h1 className="text-white tracking-tight font-medium text-lg font-geist">
+                  {title}
                 </h1>
 
                 <p className="text-[13px] mt-px">
-                  <span className="font-medium">@carlosivestors</span> on
-                  Instagram
+                  <span className="font-medium">{instagram}</span> on Instagram
                 </p>
               </div>
 
@@ -37,12 +95,18 @@ const FancyCard = () => {
                     <path d="M128,16a88.1,88.1,0,0,0-88,88c0,75.3,80,132.17,83.41,134.55a8,8,0,0,0,9.18,0C136,236.17,216,179.3,216,104A88.1,88.1,0,0,0,128,16Zm0,56a32,32,0,1,1-32,32A32,32,0,0,1,128,72Z" />
                   </svg>
 
-                  <p className="text-sm pb-px font-light">Kigali, Rwanda</p>
+                  <p className="text-sm pb-px font-light">{location}</p>
                 </div>
 
-                <p className="text-xs text-red-700 opacity-60">
-                  Oops! This property is no longer available
-                </p>
+                {status === "unavailable" ? (
+                  <p className="text-xs text-red-800">
+                    Oops! This property is no longer available
+                  </p>
+                ) : (
+                  <p className="text-xs text-green-500">
+                    Yes! This property is available
+                  </p>
+                )}
               </div>
             </div>
           </div>
@@ -51,5 +115,3 @@ const FancyCard = () => {
     </div>
   );
 };
-
-export default FancyCard;
